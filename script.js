89829513850af5e9664d44c0a6f0b31184020f81
Matheus Lourenço de Souza <107487181+matheuslsouza@users.noreply.github.com>
2022-09-28 16:31:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const btn_submit = document.getElementById("btn_submit");
-
+//ESCONDER CONTEUD NA TELA
 wrapper_submit.style.display = "none";
+
+//ESCOLHENDO VALOR DOS BUUTTON
+function insertValues(num) {
+  const btnSelect = document.getElementById("btnNumber").value;
+  document.getElementById("btnNumber").toFixed = btnSelect;
+  console.log(num);
+
+  //MOSTRAR - ESCONDER CONTEUDO NA TELA APÓS EVENDO DE CLICK
+  btn_submit.addEventListener("click", function () {
+    wrapper_submit.style.display = "block";
+    wrapper.style.display = "none";
+  });
+  document.getElementById("resultado").innerHTML = `You selected ${num}`;
+}
